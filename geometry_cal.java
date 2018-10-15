a97b221f471d8aa20bab9e2429b9dd393246d199
sam bauer <42975601+samuelb1130@users.noreply.github.com>
2018-10-15 17:54:00 -0400

    Add files via upload

--- geometry_cal.java
+++ geometry_cal.java
@@ -13,7 +13,7 @@ public class geometry_cal {
 		System.out.println("Press enter to continue..");
 		bread.nextLine();
 		System.out.println("AREA OF A TRIANGLE");
-		System.out.print("Please enter the measureof the base:	");
+		System.out.print("Please enter the measure of the base:	");
 	
 		String baseStr  = bread.nextLine();
 		System.out.print("\nPlease enter the messure of the height:	");
@@ -24,18 +24,44 @@ public class geometry_cal {
 		
 		double height = Double.parseDouble(heightStr);
 		
-		System.out.print("Please enter the unit of messure:	");
+		System.out.print("\nPlease enter the unit of messure:	");
 		String unit = bread.nextLine();
 		
 		double area = .5*(base*height);
 		
-		System.out.println("The area of the triangle is " + area + " " + unit + "^2");
+		System.out.println("\nThe area of the triangle is " + area + " " + unit + "^2");
 		
 		
+		System.out.println("Press enter to continue..");
+		bread.nextLine();		
+		
+		//================================================================================================================================
 		
 		
+		System.out.print("\n\nPlease enter you're name: ");
+		String Name2;
+		Name2 = bread.nextLine();
+		
+		System.out.println("\n\nHello, " + Name2 + ", welcome to geometry_cal!");
+
+		
+		System.out.println("PERIMETER OF A SQUARE");
+		System.out.println("\nPlease enter the measurement of the side:	");
+		String sideStr  = bread.nextLine();
+		
+		System.out.println("\nPlease enter the unit of messure:	");
+		
+		String unit2 = bread.nextLine();
+		
+		double side; 
+		side = Double.parseDouble(sideStr);
+		
+
+		double perimeter = 4*(side);
 		
 		
+		System.out.println("\nThe perimeter of the sqaure is " + perimeter + unit2);
+	
 		
 	}
 
